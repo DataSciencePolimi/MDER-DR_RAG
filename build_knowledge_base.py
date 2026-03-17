81c37227fa917007eaaf504b70f7f276ce6e28ab
Riccardo Campi <72938367+riccamper@users.noreply.github.com>
2026-03-17 16:41:11 +0100

    Change model from gpt-oss:120b to gpt-oss:20b

--- build_knowledge_base.py
+++ build_knowledge_base.py
@@ -6,7 +6,7 @@ from private_settings import PRIVATE_SETINGS
 
 # Creating and running the knowledge base class based on the environment
 if PRIVATE_SETINGS["LLM_LOCAL"]:
-    ke = KnowledgeExtractor("ollama", "gpt-oss:120b", "mxbai-embed-large")
+    ke = KnowledgeExtractor("ollama", "gpt-oss:20b", "mxbai-embed-large")
 else:
     # Online
     ke = KnowledgeExtractor("openai", "gpt-4", "text-embedding-3-small")
